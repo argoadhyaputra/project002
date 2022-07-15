8aa663a81f4129ffc0b726211074a7c74b155f54
argoadhyaputra <argoadhya505@gmail.com>
2022-07-15 20:00:57 +0700

    mobile fixed error

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-var argDropdown = document.querySelectorAll('.arg-dropdown-menu');
+var argDropdown = document.querySelectorAll('.arg-dropdown');
 var eggButton = document.querySelector('.egg')
 var dropdownArray = Array.prototype.slice.call(argDropdown, 0);
 dropdownArray.forEach(function(e) {
-    var x = e.querySelectorAll('li');
-    var k = document.querySelector('.arg-dropdown-menu');
-    var argD = document.querySelector('.arg-dropdown');
-    eggButton.onclick = (s) => {
-        if(k.classList.contains('show')) {
+    // console.log(e);
+    // var x = e.querySelector('li');
+    // var k = e.querySelector('.arg-dropdown-menu ');
+    var k = e.querySelector('i[data-toggle="dropdown"]');
+    var argD = e.querySelector('.arg-dropdown-menu');
+    k.onclick = function(y){
+        console.log(k);
+        console.log(y);
+                // console.log(this.s);
+        if(argD.classList.contains('show')) {
             e.classList.remove('show');
-            argD.classList.remove('shows');
+            // argD.classList.remove('shows');
+            console.log('ini'); 
+            argD.classList.remove('show');
+            k.classList.remove('rotate');
         } else {
+            console.log('sukses');
+            argD.classList.add('show');
             // argD.style.height = "100px";
-            argD.classList.add('shows');
+            // argD.classList.add('shows');
+            k.classList.add('rotate');
             e.classList.add('show');
-        }
-    }
+            }
+    } 
 });
 
 // console.log(dropdownArray);
